e2b87700f75f8731b193229886955e04cca87ec9
AO620619 <AO620619@itweud01991vm94>
2023-05-08 14:12:58 +0200

    Add project files.

---
+++ 8mei/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace _8mei
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World! Hoe oud ben je");
+            string name = Console.ReadLine();
+            Console.WriteLine("gefeliciteerd met je leeftijd van "+name);
+        }
+    }
+}
